b2a7c364ef6d5f83eb3823477f5b18eb5ba60b15
vquyen96 <quyendvd00557@fpt.edu.vn>
2019-11-02 15:21:26 +0700

    Add create user_profile

--- src/main/java/com/spring/dev2chuc/nutritious_food/payload/ApiResponse.java
+++ src/main/java/com/spring/dev2chuc/nutritious_food/payload/ApiResponse.java
@@ -3,10 +3,25 @@ package com.spring.dev2chuc.nutritious_food.payload;
 public class ApiResponse {
     private Boolean success;
     private String message;
+    private Object object;
 
     public ApiResponse(Boolean success, String message) {
         this.success = success;
         this.message = message;
+    }
+
+    public ApiResponse(Boolean success, String message, Object object) {
+        this.success = success;
+        this.message = message;
+        this.object = object;
+    }
+
+    public Object getObject() {
+        return object;
+    }
+
+    public void setObject(Object object) {
+        this.object = object;
     }
 
     public Boolean getSuccess() {
