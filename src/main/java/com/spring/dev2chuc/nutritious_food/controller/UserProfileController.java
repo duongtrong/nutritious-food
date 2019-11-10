6010c789ad6be5efd8990f8d4069cc72a1a32048
duongtrong <trongdvd00543@fpt.edu.vn>
2019-11-10 22:04:15 +0700

    move user profile to service

--- src/main/java/com/spring/dev2chuc/nutritious_food/controller/UserProfileController.java
+++ src/main/java/com/spring/dev2chuc/nutritious_food/controller/UserProfileController.java
@@ -1,32 +1,27 @@
 package com.spring.dev2chuc.nutritious_food.controller;
 
-import com.spring.dev2chuc.nutritious_food.model.Category;
 import com.spring.dev2chuc.nutritious_food.model.User;
 import com.spring.dev2chuc.nutritious_food.model.UserProfile;
 import com.spring.dev2chuc.nutritious_food.payload.response.ApiResponse;
 import com.spring.dev2chuc.nutritious_food.payload.UserProfileRequest;
-import com.spring.dev2chuc.nutritious_food.repository.UserProfileRepository;
+import com.spring.dev2chuc.nutritious_food.payload.response.ApiResponseError;
 import com.spring.dev2chuc.nutritious_food.service.category.CategoryService;
 import com.spring.dev2chuc.nutritious_food.service.user.UserService;
+import com.spring.dev2chuc.nutritious_food.service.userprofile.UserProfileService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AnonymousAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 @RestController
 @RequestMapping("/api/user-profile")
 public class UserProfileController {
 
     @Autowired
-    UserProfileRepository userProfileRepository;
+    UserProfileService userProfileService;
 
     @Autowired
     UserService userService;
@@ -34,39 +29,57 @@ public class UserProfileController {
     @Autowired
     CategoryService categoryService;
 
-    @PutMapping("/update")
-    public ResponseEntity<Object> update(@Valid @RequestBody UserProfileRequest userProfileRequest) {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        if (!(authentication instanceof AnonymousAuthenticationToken)) {
-            String username = authentication.getName();
-            User user = userService.findByUsername(username);
-            System.out.println(username + " == " + user.getId());
+    @PutMapping("/create")
+    public ResponseEntity<Object> createUserProfile(@Valid @RequestBody UserProfileRequest userProfileRequest) {
+        UserProfile userProfile = new UserProfile ();
+        User user = userService.getUserAuth ();
+        userProfile.setUser (user);
 
-            if (user == null) {
-                return new ResponseEntity<>(new ApiResponse(HttpStatus.NOT_FOUND.value(), "User not found"), HttpStatus.NOT_FOUND);
-            }
-            UserProfile userProfile = new UserProfile(user, userProfileRequest.getHeight(), userProfileRequest.getWeight(), userProfileRequest.getAge());
-            UserProfile userProfileResult = userProfileRepository.save(userProfile);
-            return new ResponseEntity<>(new ApiResponse<>(HttpStatus.CREATED.value(), "Create success", userProfileResult), HttpStatus.CREATED);
+        if (user == null) {
+            return new ResponseEntity<> (new ApiResponseError (HttpStatus.NOT_FOUND.value (), "User not found"), HttpStatus.NOT_FOUND);
         }
-        return new ResponseEntity<>(new ApiResponse(HttpStatus.BAD_REQUEST.value(), "You has reject"), HttpStatus.BAD_REQUEST);
+
+        UserProfile profile = userProfileService.merge (userProfile, userProfileRequest);
+        return new ResponseEntity<> (new ApiResponse<> (HttpStatus.CREATED.value (), "Save order success", profile), HttpStatus.CREATED);
+
+//        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+//        if (!(authentication instanceof AnonymousAuthenticationToken)) {
+//            String username = authentication.getName();
+//            User user = userService.findByUsername(username);
+//            System.out.println(username + " == " + user.getId());
+//
+//            if (user == null) {
+//                return new ResponseEntity<>(new ApiResponse(HttpStatus.NOT_FOUND.value(), "User not found"), HttpStatus.NOT_FOUND);
+//            }
+//            UserProfile userProfile = new UserProfile(user,
+//                    userProfileRequest.getHeight(),
+//                    userProfileRequest.getWeight(),
+//                    userProfileRequest.getAge());
+//            UserProfile userProfileResult = userProfileRepository.save(userProfile);
+//            return new ResponseEntity<>(new ApiResponse<>(HttpStatus.CREATED.value(), "Create success", userProfileResult), HttpStatus.CREATED);
+//        }
+//        return new ResponseEntity<>(new ApiResponse(HttpStatus.BAD_REQUEST.value(), "You has reject"), HttpStatus.BAD_REQUEST);
+//    }
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<?> show(@PathVariable("id") Long id) {
-        UserProfile userProfile = userProfileRepository.findById(id).orElseThrow(null);
+        UserProfile userProfile = userProfileService.getDetail (id);
         return new ResponseEntity<>(new ApiResponse<>(HttpStatus.OK.value(), "Get detail success", userProfile), HttpStatus.OK);
     }
 
     @PutMapping("/{id}/category")
-    public ResponseEntity<?> updateCategory(@Valid @RequestBody List<Long> ids, @PathVariable("id") Long id) {
-        System.out.println(ids);
-        UserProfile userProfile = userProfileRepository.findById(id).orElseThrow(null);
-        List<Category> categories = categoryService.findAllByIdIn(ids);
-        System.out.println(categories.size());
-        Set<Category> categorySet = new HashSet<>(categories);
-        userProfile.setCategories(categorySet);
-        userProfileRepository.save(userProfile);
-        return new ResponseEntity<>(new ApiResponse<>(HttpStatus.OK.value(), "Create new category success", userProfile), HttpStatus.OK);
+    public ResponseEntity<?> updateCategory(@Valid @RequestBody List<Long> ids, @RequestBody UserProfileRequest userProfileRequest, @PathVariable("id") Long id) {
+        if (ids == null) {
+            return new ResponseEntity<> (new ApiResponseError (HttpStatus.NOT_FOUND.value (), "Id not found"), HttpStatus.NOT_FOUND);
+        }
+
+        UserProfile userProfile = userProfileService.getDetail (id);
+        if (userProfile == null) {
+            return new ResponseEntity<> (new ApiResponseError (HttpStatus.NOT_FOUND.value (), "Id not found"), HttpStatus.NOT_FOUND);
+        }
+
+        UserProfile profile = userProfileService.update (userProfile, userProfileRequest);
+        return new ResponseEntity<>(new ApiResponse<>(HttpStatus.OK.value(), "Create new category success", profile), HttpStatus.OK);
     }
 }
