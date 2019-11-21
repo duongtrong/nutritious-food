db7d329c5590afdcd4e5cc886e1a8b517b21ac3e
duongtrong <trongdvd00543@fpt.edu.vn>
2019-11-21 19:24:13 +0700

    create cron schedule

--- src/main/java/com/spring/dev2chuc/nutritious_food/controller/CategoryController.java
+++ src/main/java/com/spring/dev2chuc/nutritious_food/controller/CategoryController.java
@@ -17,6 +17,7 @@ import javax.validation.Valid;
 import java.util.ArrayList;
 import java.util.List;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/api/category")
 public class CategoryController {
