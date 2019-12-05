c09c13ff02281bc202df36037f966046a4f7613a
duongtrong <trongdvd00543@fpt.edu.vn>
2019-12-05 17:47:59 +0700

    Part 2 modify cron job and firebase to service

--- src/main/java/com/spring/dev2chuc/nutritious_food/config/ScheduledTasks.java
+++ src/main/java/com/spring/dev2chuc/nutritious_food/config/ScheduledTasks.java
@@ -1,119 +1,68 @@
 package com.spring.dev2chuc.nutritious_food.config;
 
-import com.spring.dev2chuc.nutritious_food.model.Device;
-import com.spring.dev2chuc.nutritious_food.model.User;
-import com.spring.dev2chuc.nutritious_food.service.device.DeviceService;
-import com.spring.dev2chuc.nutritious_food.service.firebase.DefineMessage;
-import com.spring.dev2chuc.nutritious_food.service.firebase.PushNotificationServiceImpl;
-import com.spring.dev2chuc.nutritious_food.service.user.UserService;
-import org.json.JSONObject;
+import com.spring.dev2chuc.nutritious_food.service.firebase.CronJobService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpEntity;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
-
-import java.util.List;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutionException;
 
 @Component
 public class ScheduledTasks {
 
     @Autowired
-    DefineMessage message;
-
-    @Autowired
-    PushNotificationServiceImpl pushNotificationService;
-
-    @Autowired
-    DeviceService deviceService;
-
-    @Autowired
-    UserService userService;
+    CronJobService cronJobService;
 
     private static final Logger logger = LoggerFactory.getLogger(ScheduledTasks.class);
 
     @Scheduled(cron = "0 26 9 * * ?")
-    public void scheduleTaskWithCronToday() {
-        List<User> users = userService.findAll();
-        for (User user : users) {
-            List<Device> devices = deviceService.getByUser(user);
-            for (Device device : devices) {
-                JsonObject(device, "morning.message", "today.message");
-            }
+    public void scheduleTaskWithCronToday(String body, String title) {
+        try {
+            cronJobService.checkUserAndDevice("morning.message", "today.message");
+        } catch (Exception ignored) {
+            logger.error("Push notification error!");
         }
         logger.info("Push notification success!");
     }
 
     @Scheduled(cron = "0 0 12 * * ?")
-    public void scheduleTaskWithCronNoonday() {
-        List<User> users = userService.findAll();
-        for (User user : users) {
-            List<Device> devices = deviceService.getByUser(user);
-            for (Device device : devices) {
-                JsonObject(device, "noonday.message", "today.message");
-            }
+    public void scheduleTaskWithCronNoonday(String body, String title) {
+        try {
+            cronJobService.checkUserAndDevice("noonday.message", "today.message");
+        } catch (Exception ignored) {
+            logger.error("Push notification error!");
         }
-        logger.info(message.get ("noonday.message"));
+        logger.info("Push notification success!");
     }
 
     @Scheduled(cron = "0 45 12 * * ?")
-    public void scheduleTaskWithCronLastNoonday() {
-        List<User> users = userService.findAll();
-        for (User user : users) {
-            List<Device> devices = deviceService.getByUser(user);
-            for (Device device : devices) {
-                JsonObject(device, "last.noonday.message", "today.message");
-            }
+    public void scheduleTaskWithCronLastNoonday(String body, String title) {
+        try {
+            cronJobService.checkUserAndDevice("last.noonday.message", "today.message");
+        } catch (Exception ignored) {
+            logger.error("Push notification error!");
         }
-        logger.info(message.get ("last.noonday.message"));
+        logger.info("Push notification success!");
     }
 
     @Scheduled(cron = "0 0 19 * * ?")
-    public void scheduleTaskWithCronDinner() {
-        List<User> users = userService.findAll();
-        for (User user : users) {
-            List<Device> devices = deviceService.getByUser(user);
-            for (Device device : devices) {
-                JsonObject(device, "dinner.message", "today.message");
-            }
+    public void scheduleTaskWithCronDinner(String body, String title) {
+        try {
+            cronJobService.checkUserAndDevice("dinner.message", "today.message");
+        } catch (Exception ignored) {
+            logger.error("Push notification error!");
         }
-        logger.info(message.get ("dinner.message"));
+        logger.info("Push notification success!");
+
     }
 
     @Scheduled(cron = "0 30 19 * * ?")
-    public void scheduleTaskWithCronLastDinner() {
-        List<User> users = userService.findAll();
-        for (User user : users) {
-            List<Device> devices = deviceService.getByUser(user);
-            for (Device device : devices) {
-                JsonObject(device, "last.dinner.message", "today.message");
-            }
-        }
-        logger.info(message.get ("last.dinner.message"));
-    }
-
-    private void JsonObject(Device device, String body, String title) {
-        JSONObject bodies = new JSONObject();
-        bodies.put("to", device);
-        bodies.put("collapse_key", "type_a");
-
-        JSONObject notification = new JSONObject();
-        notification.put("body", message.get(body));
-        notification.put("title", message.get(title));
-
-        bodies.put("notification", notification);
-
-        HttpEntity<String> request = new HttpEntity<>(body.toString());
-        CompletableFuture<String> pushNotification = pushNotificationService.send(request);
-        CompletableFuture.allOf(pushNotification).join();
-
+    public void scheduleTaskWithCronLastDinner(String body, String title) {
         try {
-            String firebaseResponse = pushNotification.get();
-        } catch (InterruptedException | ExecutionException e) {
-            e.printStackTrace();
+            cronJobService.checkUserAndDevice("last.dinner.message", "today.message");
+        } catch (Exception ignored) {
+            logger.error("Push notification error!");
         }
+        logger.info("Push notification success!");
     }
 }
--- src/main/java/com/spring/dev2chuc/nutritious_food/controller/PushNotificationController.java
+++ src/main/java/com/spring/dev2chuc/nutritious_food/controller/PushNotificationController.java
@@ -1,11 +1,8 @@
 package com.spring.dev2chuc.nutritious_food.controller;
 
-import com.spring.dev2chuc.nutritious_food.config.ScheduledTasks;
 import com.spring.dev2chuc.nutritious_food.model.Device;
 import com.spring.dev2chuc.nutritious_food.model.User;
-import com.spring.dev2chuc.nutritious_food.payload.DeviceRequest;
 import com.spring.dev2chuc.nutritious_food.payload.response.ApiResponseError;
-import com.spring.dev2chuc.nutritious_food.payload.response.DeviceDTO;
 import com.spring.dev2chuc.nutritious_food.service.device.DeviceService;
 import com.spring.dev2chuc.nutritious_food.service.firebase.PushNotificationServiceImpl;
 import com.spring.dev2chuc.nutritious_food.service.user.UserService;
@@ -15,7 +12,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -35,7 +31,7 @@ public class PushNotificationController {
     @Autowired
     UserService userService;
 
-    @RequestMapping(value = "/send", method = RequestMethod.GET, produces = "application/json")
+    @GetMapping("/send")
     public ResponseEntity<?> send() throws JSONException {
 
         List<User> users = userService.findAll();
@@ -43,7 +39,7 @@ public class PushNotificationController {
             List<Device> devices = deviceService.getByUser(user);
             for (Device device : devices) {
                 JSONObject body = new JSONObject();
-                body.put("to", "faKUFdp5bo0:APA91bGyuiI9K070mPjeNa0fGhHNR4DbO64_c10QvwkjXMafJ_-oRpRoe39avwDKinJbq0ODQCSrCXm9T-ANLZRYUMS3S9EwEjiybMEJYf1o0zWxI6KJO-je8fiWHwiIIHqur_DbQ0kn");
+                body.put("to", device);
                 body.put("collapse_key", "type_a");
 
                 JSONObject notification = new JSONObject();
@@ -53,9 +49,7 @@ public class PushNotificationController {
                 body.put("notification", notification);
 
                 HttpEntity<String> request = new HttpEntity<>(body.toString());
-                System.out.println(request);
                 CompletableFuture<String> pushNotification = pushNotificationService.send(request);
-                System.out.println(pushNotification);
                 CompletableFuture.allOf(pushNotification).join();
 
                 try {
